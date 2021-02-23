781278356dd8bff612301e0b03d7b68e5473d96e
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-02-23 22:01:51 +0000

    Manager Test file created and passing

---
+++ __tests__/manager.test.js
@@ -0,0 +1,29 @@
+// Obtain / Require Intern Class
+const Manager = require('../lib/manager');
+
+// Intern Class Constructor Jest test
+describe('Manager', () => {
+    it('Should set the office number using constructor', () => {
+        const testVal = 10;
+        const manager = new Manager('Manager', 1, 'test@test.com', testVal);
+        expect(manager.officeNumber).toBe(testVal);
+    });
+});
+
+// getRole() Method Jest test
+describe('getRole', () => {
+    it('Should return \"Manager\" using getRole()', () => {
+        const testVal = 'Manager';
+        const manager = new Manager('Role', 1, 'test@test.com', 10);
+        expect(manager.getRole()).toBe(testVal);
+    });
+});
+
+// getOfficeNumber() Method Jest test
+describe('getOfficeNumber', () => {
+    it('Should get the office number using getOffice()', () => {
+        const testVal = 10;
+        const manager = new Manager('Office', 1, 'test@test.com', testVal);
+        expect(manager.getOfficeNumber()).toBe(testVal);
+    });
+});
