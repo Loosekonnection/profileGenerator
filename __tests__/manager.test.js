85a32a0756f2f254952d03cb1d4a0f8bfdf7e8ae
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-02-25 14:21:00 +0000

    code refactor

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -4,26 +4,26 @@ const Manager = require('../lib/manager');
 // Intern Class Constructor Jest test
 describe('Manager', () => {
     it('Should set the office number using constructor', () => {
-        const testVal = 10;
-        const manager = new Manager('Manager', 1, 'email@test.co.uk', testVal);
-        expect(manager.officeNumber).toBe(testVal);
+        const testOffice = 10;
+        const manager = new Manager('Manager', 1, 'email@test.co.uk', testOffice);
+        expect(manager.officeNumber).toBe(testOffice);
     });
 });
 
 // getRole() Method Jest test
 describe('getRole', () => {
     it('Should return \"Manager\" using getRole()', () => {
-        const testVal = 'Manager';
+        const testRole = 'Manager';
         const manager = new Manager('Role', 1, 'email@test.co.uk', 10);
-        expect(manager.getRole()).toBe(testVal);
+        expect(manager.getRole()).toBe(testRole);
     });
 });
 
 // getOfficeNumber() Method Jest test
 describe('getOfficeNumber', () => {
-    it('Should get the office number using getOffice()', () => {
-        const testVal = 10;
-        const manager = new Manager('Office', 1, 'email@test.co.uk', testVal);
-        expect(manager.getOfficeNumber()).toBe(testVal);
+    it('Should get the office number using getOfficeNumber()', () => {
+        const testOfficeNo = 10;
+        const manager = new Manager('Office', 1, 'email@test.co.uk', testOfficeNo);
+        expect(manager.getOfficeNumber()).toBe(testOfficeNo);
     });
 });
